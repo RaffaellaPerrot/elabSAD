96e361b1dc311e0164a93bfeefcab42b05f1ebd1
Anna Lamboglia <annalamboglia99@gmail.com>
2021-09-08 22:11:30 +0200

    Ho creato un nuovo file per la lista pazienti

--- Lista Pazienti.php
+++ Lista Pazienti.php
@@ -5,7 +5,7 @@
 
   <?php require "./includes/header.html" ?>
 
-  <title>Lista Pazienti</title>
+  <title>INSERIRE TITOLO PAGINA</title>
 
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
